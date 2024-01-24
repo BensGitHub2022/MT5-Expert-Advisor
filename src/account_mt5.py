928f4fc1fdc6c359e655a1839697f1e113e7016d
Ben Feldman <bfeldman2016@gmail.com>
2024-01-23 21:23:07 -0800

    Making changes per review: -Removed symbol injection from account_mt5 -Added shared_helper_functions

--- src/account_mt5.py
+++ src/account_mt5.py
@@ -7,9 +7,6 @@ class AccountMT5(IAccount):
 
     balance: float
     profit: float
-
-    ticket: int # do we use this ticket variable in live implementation ? 1.21.24 - to decide
-    symbol: object # should we pass a symbol object to this class ? 1.21.24 - implemented in sim class, need to add to live implementation
     
     positions_df: pd.DataFrame
     positions: dict
@@ -18,8 +15,6 @@ class AccountMT5(IAccount):
         self.balance = self.get_account_balance()
         self.profit = self.get_account_profit()
         self.positions = dict()
-        # self.positions_df = pd.DataFrame() # Not used
-        self.symbol = symbol # Added to meet requirements of factory method and interface, currently not used. Will be updated when more dependency injection is added
         
     def get_account_info(self) -> dict:
         account_info_dict = mt5.account_info()._asdict()
---
+++ src/shared_helper_functions.py
@@ -0,0 +1,4 @@
+RISK = .02
+
+def calc_risk_per_trade(balance: float) -> float:
+        return balance * RISK

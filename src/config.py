037b3c6c7245d2616a48f310574ff61deb27772e
Saralyn Ogden <Saralyn.Ogden@gmail.com>
2024-05-15 17:30:36 -0700

    fix config

--- src/config.py
+++ src/config.py
@@ -9,16 +9,17 @@ class Config():
     credentials: dict
     timeframe: str
 
-    json_reader: JsonReader
-
     def __init__(self, symbol_name: str) -> None:
-        self.filepath: str = "config/" + symbol_name + ".json"
-        self.json_reader = JsonReader(self.filepath)
-        self.timeframe = self.json_reader.get_timeframe()
-        
+        json_reader: JsonReader
         if not production:
-            self.candlesticks_filepath = self.json_reader.get_symbol_candlesticks_filepath()
-            self.ticks_filepath = self.json_reader.get_symbol_ticks_filepath()
+            self.filepath: str = "config/" + symbol_name + ".json"
+            json_reader = JsonReader(self.filepath)
+            self.candlesticks_filepath = json_reader.get_symbol_candlesticks_filepath()
+            self.ticks_filepath = json_reader.get_symbol_ticks_filepath()
         else:
+            self.filepath = "config/settings.json"
+            json_reader = JsonReader(self.filepath)
             self.candlesticks_filepath = ""
             self.ticks_filepath = ""
+
+        self.timeframe = json_reader.get_timeframe()
